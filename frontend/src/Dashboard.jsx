a5cf3a6362af70839569b4d348023a1a95ada812
Vasanth Chandrasekar <35191203+Vasanth14@users.noreply.github.com>
2024-08-07 00:12:11 +0530

    dashboard work

--- frontend/src/Dashboard.jsx
+++ frontend/src/Dashboard.jsx
@@ -1,7 +1,8 @@
-import { Image } from "react-bootstrap";
+import { Button, Image, Table } from "react-bootstrap";
 import LeftSideMenu from "./components/LeftSideMenu";
 import UserNav from "./components/UserNav";
 import calendar from "../src/assets/images/calendar.svg";
+import avatar from '../src/assets/images/profile.png'
 
 import React from "react";
 
@@ -15,7 +16,7 @@ const Dashboard = () => {
           <div className="pagetitlediv">
             <h2 className="title">Welcome, Dr. Stephen!</h2>
           </div>
-          <div className="flexbox">
+          <div className="flexbox analyticscard my-3">
             <div className="cards appointmentcard">
               <div className="table-content">
                 <div>
@@ -61,6 +62,116 @@ const Dashboard = () => {
                   <h4 className="sub-heading">28.8k</h4>
                   <p className="content">Video Consulting</p>
                 </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="flexbox opanalytics my-4">
+            <div className="oprequest">
+              <div className="cardtitle">
+                <h2>Appointment Request</h2>
+              </div>
+              <div className="contentbox">
+                <Table hover>
+                  <tbody>
+                    <tr>
+                      <td>
+                        <div className="table-content">
+                            <div>
+                              <Image src={avatar} className="profileimg"/>
+                            </div>
+                            <div className="appoitmentcontent">
+                              <h2>Arjun</h2>
+                              <p>45 Male, 12 April 9:30</p>
+                            </div>
+                        </div>
+                      </td>
+                      <td>
+                        <Button className="btn declinebtn">Declined</Button>
+                      </td>
+                    </tr>
+                    <tr>
+                    <td>
+                        <div className="table-content">
+                            <div>
+                              <Image src={avatar} className="profileimg"/>
+                            </div>
+                            <div className="appoitmentcontent">
+                              <h2>John</h2>
+                              <p>45 Male, 12 April 9:30</p>
+                            </div>
+                        </div>
+                      </td>
+                      <td>
+                        <Button className="btn declinebtn">Declined</Button>
+                      </td>
+                    </tr>
+                    <tr>
+                    <td>
+                        <div className="table-content">
+                            <div>
+                              <Image src={avatar} className="profileimg"/>
+                            </div>
+                            <div className="appoitmentcontent">
+                              <h2>Dani</h2>
+                              <p>45 Male, 12 April 9:30</p>
+                            </div>
+                        </div>
+                      </td>
+                      <td>
+                        <Button className="btn confirmbtn">Confirmed</Button>
+                      </td>
+                    </tr>
+                  </tbody>
+                </Table>
+              </div>
+            </div>
+
+            <div className="patientrecord">
+              <div className="cardtitle">
+                <h2>Appointment Request</h2>
+              </div>
+              <div className="contentbox">
+                <Table hover>
+                  <tbody>
+                    <tr>
+                      <td>Otto</td>
+                      <td>@mdo</td>
+                    </tr>
+                    <tr>
+                      <td>Thornton</td>
+                      <td>@fat</td>
+                    </tr>
+                    <tr>
+                      <td>Thornton</td>
+                      <td>@fat</td>
+                    </tr>
+                  </tbody>
+                </Table>
+              </div>
+            </div>
+
+            <div className="tdyappointment">
+              <div className="cardtitle">
+                <h2>Appointment Request</h2>
+              </div>
+              <div className="contentbox">
+                <Table hover>
+                  <tbody>
+                    <tr>
+                      <td>Otto</td>
+                      <td>@mdo</td>
+                    </tr>
+                    <tr>
+                      <td>Thornton</td>
+                      <td>@fat</td>
+                    </tr>
+                    <tr>
+                      <td>Thornton</td>
+                      <td>@fat</td>
+                    </tr>
+                  </tbody>
+                </Table>
               </div>
             </div>
           </div>
